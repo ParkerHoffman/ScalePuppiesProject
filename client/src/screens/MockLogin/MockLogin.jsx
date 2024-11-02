2d9a582e37d2a25d1b27ae9e197264f23638e2b9
JlGremard <jaimielgremard@student.cofo.edu>
2024-11-02 17:21:08 -0500

    Log in complete

--- client/src/screens/MockLogin/MockLogin.jsx
+++ client/src/screens/MockLogin/MockLogin.jsx
@@ -28,18 +28,31 @@ export default function MockLogin({onLogin}) {
                 <div class="logInForm"> {/*log in form*/}
                     <h4 class="logInText">Log In</h4>
                     <form class="logInFields">
-                        <FloatLabel class="farmName">
+                        {/*<FloatLabel class="farmName">
                             <InputText id="farmName"/>
-                            <label htmlFor="farmName">Farm Name</label>
+                            <label htmlFor="farmName">Farm Name:</label>
                         </FloatLabel>
                         <FloatLabel class="username">
                             <InputText id="username" />
-                            <label htmlFor="username">Username</label>
+                            <label htmlFor="username">Username:</label>
                         </FloatLabel>
                         <FloatLabel class="password">
                             <Password id="password"/>
-                            <label htmlFor="password">Password</label>
+                            <label htmlFor="password">Password:</label>
                         </FloatLabel>
+                        */}
+                        <div class="farmName">
+                            <label class="farmNameLabel" htmlFor="farmName">Farm Name:</label>
+                            <InputText class="farmNameInput" id="farmName" placeholder="Farm Name" />
+                        </div>
+                        <div class="username">
+                            <label class="usernameLabel" htmlFor="username">Username:</label>
+                            <InputText class="usernameInput" id="username" placeholder="Username" />
+                        </div>
+                        <div class="password">
+                            <label class="passwordLabel" htmlFor="password">Password:</label>
+                            <Password class="passwordInput" id="password" placeholder="Password"/>   
+                        </div>
                         <Button class="submit" label="Submit" onClick={(e) => handleSubmit()}/>
                     </form>
                 </div>
--- client/src/screens/logged-in/Dashboard/Dashboard.jsx
+++ client/src/screens/logged-in/Dashboard/Dashboard.jsx
@@ -1,17 +1,36 @@
-
+import { Avatar } from 'primereact/avatar';
+import { Menubar } from 'primereact/menubar';
+import { Toast } from 'primereact/toast';
+import { ConfirmDialog } from 'primereact/confirmdialog'; // For <ConfirmDialog /> component
+import { confirmDialog } from 'primereact/confirmdialog'; // For confirmDialog method
+import { PanelMenu } from 'primereact/panelmenu';
+import { Menu } from 'primereact/menu';
+import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
+import { Button } from 'primereact/button';
 
 export default function Dashboard() {
     return (
         <>
             <div class="dashboardScreen">
                 <div class="header">
+                    <div class="siteInfo">
                     <h1 class="headerText">HerdHarmony</h1>
                     <p class="motto">Created to create harmony in and between your herds</p>
+                    </div>
+                    <Avatar class="profileAvatar" label="T" style={{backgroundColor: '#FFFFFF', color:'#000000'}} shape="circle"/>
+                    <Menu class="profileMenu" />
                 </div>
 
                 <div class ="wrapper">
-                    <div class="content"> main content </div>
-                    <div class="navigation"> navigation </div>
+                    <div class="content"> main content 
+                        <DataView id="herdTable" class="herdTable" label="Herds"/>
+                        <Button id="viewInfoButton" class="viewInfoButton" label="View Information" />
+                        <Button id="newHerdButton" class="newHerdButton" label="Add New Herd" />
+                        <Button id="deleteHerdButton" class="deleteHerdButton" label="Delete Herd" />
+                    </div>
+                    <div class="navigation"> navigation
+                        <PanelMenu class="navigationMenu" className="w-full md:w-20rem" />
+                    </div>
                 </div>
             </div>
         </>
