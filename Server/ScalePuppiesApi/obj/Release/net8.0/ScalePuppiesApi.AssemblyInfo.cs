f67217c702b8e110a0c0c9bbb12cd36e9569392b
Cpt-Crash <145154783+Cpt-Crash@users.noreply.github.com>
2024-11-03 01:23:54 -0600

    LateCommit
    Created new branch after nearly losing multiple folders.

--- Server/ScalePuppiesApi/obj/Release/net8.0/ScalePuppiesApi.AssemblyInfo.cs
+++ Server/ScalePuppiesApi/obj/Release/net8.0/ScalePuppiesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScalePuppiesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d0e62cbb47ccc035f963da42e5c4b76285259d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb508971461b173c46ee6bf4b57622dfaf5d980d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScalePuppiesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScalePuppiesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
